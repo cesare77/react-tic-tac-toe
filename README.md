dc0496b0a3ea54f8aeaa927ef1cd0bcec9af03e6
fiamingoc <cesare.fiamingo.external@ynap.com>
2024-10-28 17:10:40 +0100

    app + two extra points improvements

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # react-tic-tac-toe
-React tic-tac-toe game from react-dev official learning page
+
+React tic-tac-toe game from react-dev [official learning page](https://react.dev/learn/tutorial-tic-tac-toe)
+
+## Git commands overview
+
+- `git remote add origin <repository url>` to sync project to remote
+- `git push --force -u -origin master` to push to remote
+- `git add .` to add changes to commit
+- `git commit -m "message"` to do commit with message
+- `git push -u origin master` to push changes to origin
+- `git log --oneline` to view commit logs
+- `git status` to view current git status
+
+## Extra improvements
+
+1. For the current move only, show “You are at move #…” instead of a button.
+2. Rewrite Board to use two loops to make the squares instead of hardcoding them.
+3. Add a toggle button that lets you sort the moves in either ascending or descending order.
+4. When someone wins, highlight the three squares that caused the win (and when no one wins, display a message about the result being a draw).
+5. Display the location for each move in the format (row, col) in the move history list.
---
+++ src/App.js
@@ -0,0 +1,140 @@
+import { useState } from "react";
+
+/**
+ * Square component
+ */
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+/**
+ * Board component.
+ */
+function Board({ xIsNext, squares, onPlay }) {
+  const matrixSize = 3;
+
+  const handleClick = (i) => {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    const nextSquares = squares.slice();
+
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+
+    onPlay(nextSquares);
+  };
+
+  const winner = calculateWinner(squares);
+  let status;
+
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next player: ${xIsNext ? "X" : "O"}`;
+  }
+
+  return (
+    <>
+      <div className="status">{status}</div>
+
+      {Array.from({ length: matrixSize }).map((row, i) => (
+        <div className={`board-row-${i}`} key={`row-${i}`}>
+          {Array.from({ length: matrixSize }).map((col, j) => (
+            <Square
+              key={`square-${i * matrixSize + j}`}
+              value={squares[i * matrixSize + j]}
+              onSquareClick={() => handleClick(i * matrixSize + j)}
+            />
+          ))}
+        </div>
+      ))}
+    </>
+  );
+}
+
+/**
+ * Game component.
+ */
+export default function game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  const handlePlay = (nextSquares) => {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  };
+
+  const jumpTo = (nextMove) => {
+    setCurrentMove(nextMove);
+  };
+
+  const moves = history.map((squares, move) => {
+    let description;
+
+    if (move > 0 && move !== currentMove) {
+      description = `Go to move #${move}.`;
+    } else if (move === currentMove) {
+      description = `You are at move #${move}`;
+    } else {
+      description = "Go to game start.";
+    }
+
+    return (
+      <li key={move} className="moves-history-item">
+        {move === currentMove ? (
+          <div>{description}</div>
+        ) : (
+          <button onClick={() => jumpTo(move)}>{description}</button>
+        )}
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol className="moves-history-list">{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+/**
+ * ...
+ */
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0, len = lines.length; i < len; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+}
